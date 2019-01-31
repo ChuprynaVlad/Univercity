38cbe5197ae4724be089c5a581f4dd27e7d15324
ChuprynaVlad <47174621+ChuprynaVlad@users.noreply.github.com>
2019-01-31 22:20:34 +0200

    Add files via upload

---
+++ Pifagor/Pifagor/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Pifagor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] line1 = new int[9];
+            int[] line2 = new int[9];
+            string Tabl = "";
+
+            for (int i = 0; i < 9; i++)
+            {
+                line1[i] = i + 1;
+                line2[i] = i + 1;
+            }
+
+
+            for (int i = 0; i < line2.Length; i++)
+            {
+                for (int z = 0; z < line1.Length; z++)
+                {
+                    Tabl += "\t" + line2[i] * line1[z];
+                }
+                Tabl += "\n";
+            }
+
+            Console.WriteLine(Tabl);
+
+            Console.ReadKey();
+        }
+    }
+}
