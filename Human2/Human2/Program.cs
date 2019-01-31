906cb9595f69b6df23af7ca29f5fe0916821ae77
ChuprynaVlad <47174621+ChuprynaVlad@users.noreply.github.com>
2019-01-31 22:38:20 +0200

    Add files via upload

---
+++ Human2/Human2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human tom = new Human();
+            tom.name = "Tom";
+            tom.age = 22;
+            tom.sex = "male";
+            tom.weight = 75;
+            tom.Info();
+
+                Console.ReadKey();
+          
+        }
+    }
+}
+class Human
+{
+   public string name;
+    public int age;
+    public string sex;
+    public int weight;
+    
+
+
+
+
+    public void Info()
+    {
+        Console.WriteLine($"{name} - {age} - {sex} - {weight}");
+    }
+}
