6bd502aedaef60ab785c3832fe2ec0d7921f6bb8
ChuprynaVlad <47174621+ChuprynaVlad@users.noreply.github.com>
2019-01-31 22:30:01 +0200

    Add files via upload

---
+++ Car/Car/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    class Program
+    {
+      
+            static void Main(string[] args)
+            {
+                Car auto = new Car();
+            auto.namemark = "BMW";
+            auto.age = 10 ;
+            auto.type = "Saloon";
+            auto.weight = 2;
+            auto.color = "grey";
+            auto.Info();
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+    class Car
+    {
+        public string namemark;
+        public int age;
+        public string type;
+        public int weight;
+        public string color;
+
+
+
+
+
+
+
+        public void Info()
+        {
+            Console.WriteLine($"{namemark} - {age} - {type} - {weight} - {color}");
+        }
+    }
+
